a6b68619d31dba73bfd04540d6f786d02fb72e66
OwenSW08 <123748830+OwenSW08@users.noreply.github.com>
2025-04-24 21:52:24 -0400

    Update friends_not_logged_in.php

--- cs4640-dev-environment-s25v1/web/src/project/templates/friends_not_logged_in.php
+++ cs4640-dev-environment-s25v1/web/src/project/templates/friends_not_logged_in.php
@@ -1,1 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="friends page">
+        <meta name="author" content="You Gao">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta property="og:title" content="main game page">
+        <meta property="og:type" content="website">
+        <meta property="og:img" content="../project/assets/mage.png">
+        <meta property="og:url" content="https://cs4640.cs.virginia.edu/kpb8hp/project/">
+        <meta property="og:description" content="text based video game rpg">
+        <meta property="og:site_name" content="Video Game Name">
+        <title>Cool RPG Game</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="../project/styles/game.css">
+        <script>
+            async function fillUsers() {
+                let input = document.querySelector('input[name="username"]').value;
+                if (input.length < 3) {
+                    return; // Don't send a request if the input is less than 3 characters
+                }
+                const response = await fetch(`?command=searchF&name=${input}`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+                const data = await response.json();
+                const datalist = document.getElementById('users');
+                datalist.innerHTML = '';
+                for (let i = 0; i < data.user_characters.length; i++) {
+                   // data[i].username is the username of the character
+                    console.log(data.user_characters[i].username);
+                    const option = document.createElement('option');
+                    option.value = data.user_characters[i].username;
+                    option.textContent = data.user_characters[i].username; // Set the text content to the username
+                    datalist.appendChild(option);
 
+                }
+            }
+        </script>
+    </head>  
+    <body>
+        <header class = "row">
+            <nav class="navbar navbar-light bg-light">
+                <a class="navbar-brand btn" href="?command=home">
+                  <h2> Cool RPG Game</h2>
+                </a>
+                <div class="n-item">
+                    <form action="?command=game" method="post">
+                        <input type="hidden" name="location" value="main">
+                        <button type="submit" class="btn n-item">Map</button>
+                    </form> 
+                </div>
+                <div class="n-item">
+                <a href="?command=inventory">Inventory</a>
+                </div>
+                <div class="n-item">
+                <a href="?command=friends">Friends</a>
+                </div>
+                <a class="nav-item align-right btn" href="?command=logout">
+                    <h3> Log out</h3>
+                </a>
+            </nav>
+        </header>
+        <div class="row">
+          <p>Exit to Welcome screen and log in to add Friends</p>
+        </div>
+        <div class="container">
+            <footer class="py-3 my-4">
+              <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                <li class="nav-item"><a href="?command=logout" class="nav-link px-2 text-body-secondary">Home</a></li>
+                <li class="nav-item"><a href="?command=game" class="nav-link px-2 text-body-secondary">Game</a></li>
+                <li class="nav-item"><a href="?command=inventory" class="nav-link px-2 text-body-secondary">Inventory</a></li>
+                <li class="nav-item"><a href="?command=friends" class="nav-link px-2 text-body-secondary">Friends</a></li>
+              </ul>
+              <small class="copyright">&copy; copyright 2025 video game name</small>
+            </footer>
+          </div>
+    </body>
+</html>
